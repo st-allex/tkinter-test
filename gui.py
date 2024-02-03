53b0be58a39884a3831f1570cb25ca6252d5483a
allex <st-allex@yandex.ru>
2024-02-03 13:28:23 +0300

    first test step

--- gui.py
+++ gui.py
@@ -1,9 +1,18 @@
 #https://www.youtube.com/watch?v=2Z8qRJ7Lqag
 import tkinter
+import requests
+from io import BytesIO
+## В Linux установка PIL начинается с установки нескольких зависимых пакетов.
+## В терминале выполните следующую команду для установки этих пакетов:
+##    sudo apt-get install libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev
+##Источник: https://uchet-jkh.ru/i/kak-ustanovit-pil-posagovaya-instrukciya
+from PIL import Image, ImageTk
 
+#tkinter.Tk.
 def btncmd():
     ent.insert(tkinter.END, "!")
     print(ent.get())
+    load_img()
 
 
 mywnd = tkinter.Tk()
@@ -49,6 +58,22 @@ ent.pack()
 ent.insert(0, 'hello')
 ent.insert(tkinter.END, ' World!')
 
-###https://www.youtube.com/watch?v=w-GT7FmwGfI&list=PL9aGGxgLOVw5oEXMk8Qhg3RVWY2dtyw8H&index=9
+
+
+
+l_urlPic = tkinter.Label(mywnd)
+l_urlPic.pack()
+
+pic_url = 'https://static.vecteezy.com/system/resources/previews/019/564/261/original/happy-chinese-new-year-2024-year-of-the-dragon-zodiac-vector.jpg'
+
+def load_img():
+    response = requests.get(pic_url)
+    if response.status_code!=200:
+        lbl['text'] = 'immage not found'
+    else:
+        img_from_url = ImageTk.PhotoImage(Image.open(BytesIO(response.content)).resize((100, 200), Image.ANTIALIAS))
+        l_urlPic.config(image=img_from_url)
+        l_urlPic.image = img_from_url
+
 
 mywnd.mainloop()
