5b3782a70585a148160f7c051e917af953ea7a0c
allex <st-allex@yandex.ru>
2024-02-11 22:20:17 +0300

    new step

--- combobox_test.py
+++ combobox_test.py
@@ -7,14 +7,18 @@ my_dict={'A':5,'B':4,'C':3,'D':3,'E':1,'F':0}
 months=list(my_dict.keys()) # All keys are taken as options
 font1=('Times',18,'normal')
 def my_upd(*args):
-    str1.set(sel.get()) #  Key name A B C D is displayed
-    str2.set(my_dict[sel.get()]) # value of the key is displayed
+    #l1.insert(0,'lsdhfs')
+    #str1.set(sel.get()) #  Key name A B C D is displayed
+    #str2.set(my_dict[sel.get()]) # value of the key is displayed
+    print('sdsdsd')
 
 sel=tk.StringVar() # string variable for the Combobox
 cb1=ttk.Combobox(my_w,values=months,width=7,
     textvariable=sel,font=font1)
 cb1.grid(row=1,column=1,padx=10, pady=20)
 sel.set(months[0])
+print(type(sel))
+sel.trace('w', my_upd)
 
 str1=tk.StringVar() # for entry1
 str2=tk.StringVar() # for entry2
@@ -25,5 +29,5 @@ l1.grid(row=1,column=2,padx=5)
 l2=tk.Entry(my_w,font=font1,width=5,textvariable=str2)
 l2.grid(row=1,column=3,padx=5)
 
-sel.trace('w',my_upd)
+
 my_w.mainloop()  # Keep the window open
